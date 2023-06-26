334a4b961c317ddc6001d0630cf97eef91a1a193
alessandroprudencio <alessandroconectado@gmail.com>
2023-06-25 22:20:06 -0400

    refactor: remove unused code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -127,26 +127,26 @@ resource "aws_eks_node_group" "node-1" {
   ]
 }
 
-# resource "aws_eks_node_group" "node-2" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "node-2"
-#   node_role_arn   = aws_iam_role.node.arn
-#   subnet_ids      = var.subnet_ids
+resource "aws_eks_node_group" "node-2" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node-2"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
 
-#   scaling_config {
-#     desired_size = var.desired_size
-#     max_size     = var.max_size
-#     min_size     = var.min_size
-#   }
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
 
-#   instance_types = ["t3.micro"]
+  instance_types = ["t3.micro"]
 
 
-#   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-#   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-#   depends_on = [
-#     aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
-#   ]
-# }
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
